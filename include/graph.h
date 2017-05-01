65f9fe6cffc42166ef93afa076205c85c1e35a2c
thejuno <enmelu@gmail.com>
2017-04-30 18:42:04 -0600

    Update graph.h

--- include/graph.h
+++ include/graph.h
@@ -14,6 +14,8 @@ struct vertex {
 	/* Links for vertex set data structure in memory */
 	struct vertex *prev;
 	struct vertex *next;
+	struct vertex *start;
+	struct vertex *neighbor;
 
 	/* Vertex tuple */
 	tuple_t tuple;
@@ -31,6 +33,7 @@ struct edge {
 	tuple_t tuple;
 };
 
+/* Genernal graph */
 struct graph {
 	struct vertex *v;	/* Vertices */
 	schema_t sv;		/* Vertex schema */
@@ -41,10 +44,23 @@ struct graph {
 	struct graph *next;
 };
 
+/* Normal form graph */
+struct cst_graph {
+	struct vertext *v;	/* Vertices */
+	schema_t sv;		/* Vertex schema */
+	struct edge *e;		/* Edges */
+	schema_t se;		/* Edge schema */
+	enum_list_t el;		/* List of enums */
+
+	struct cst_graph *next;
+};
+
 typedef struct vertex *vertex_t;
 typedef struct edge *edge_t;
 typedef struct graph *graph_t;
+typedef struct cst_graph *cst_graph_t;
 
+extern cst_graph_t cst_graphs;
 
 void vertex_init(vertex_t v);
 void vertex_set_id(vertex_t v, vertexid_t id);
@@ -61,5 +77,16 @@ edge_t graph_find_edge_by_ids(graph_t g, vertexid_t id1, vertexid_t id2);
 void graph_insert_edge(graph_t g, edge_t e);
 void graph_print(graph_t g, int with_tuples);
 
-
+/* Applying Dijkstra's algorithm to store user data */
+void cst_graph_init(cst_graph_t cst_g);
+vertex_t cst_find_source_node(graph_t g);
+bool check_tuple(edge_t e_start, edge_t e_next);
+vertex_t cst_find_neighbor_node(vertex_t v, graph_t g);
+void cst_complete_edges(graph_t g);
+void cst_complete_vertices(graph_t g);
+void remove_edge(graph_t g, edge_t e);
+void remove_vertex(graph_t g, vertex_t v);
+void cst_graph_complete_vertex(cst_graph_t g, vertex_t v);
+void cst_graph_complete_edge(cst_graph_t g, edge_t e);
+void cst_grow(vertex_t v, graph_t g);
 #endif
